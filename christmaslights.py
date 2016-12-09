e1052c46b10cad2cf54e6c703f19921afe4a6913
Alvin Gao <gaovinal@gmail.com>
2016-12-08 22:46:22 -0500

    Changed how commands are handled.

--- christmaslights.py
+++ christmaslights.py
@@ -1,23 +1,26 @@
 import serial
 from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
+ard = serial.Serial("/dev/ttyACM0", 9600);
+msg = ard.readline();
+print(msg);
 
 @app.route('/', methods = ['POST','GET'])
 def index():
-    ard = serial.Serial("/dev/ttyACM0", 9600);
-    msg = ard.readline();
-    print(msg);
-
-    if request.method == 'POST':
-        if request.form['submit'] == 'rainbow':
-            ard.write("rainbow\n")
-        elif request.form['submit'] == 'strobe':
-            ard.write("christmas\n")
-        else:
-            pass
     return render_template('index.html')
     if __name__ == "__main__":
         # lets launch our webpage!
         # do 0.0.0.0 so that we can log into this webpage
         # using another computer on the same network later
         app.run(host='0.0.0.0')
+
+@app.route('/<cmd>')
+def command(cmd):
+    if cmd == "rainbow":
+        ard.write("rainbow\n")
+    elif cmd == "christmas":
+        rd.write("christmas\n")
+    else":
+        pass
+
+    return "Wrote %s" % cmd
